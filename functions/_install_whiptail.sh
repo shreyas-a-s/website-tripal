3f640f015b68022d4f5b80bec1eb2865260bbf0a
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-09-12 20:56:10 +0530

    feat: Disable verbose output if whiptail is already installed

--- functions/_install_whiptail.sh
+++ functions/_install_whiptail.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 
 function _install_whiptail {
-  if command -v whiptail > /dev/null; then # Do nothing if whiptail is already installed
-    printf "\nWhiptail is already installed\n"
-  else # Install whiptail
+  if ! command -v whiptail > /dev/null; then # Install whiptail if it is NOT already installed
     if command -v apt-get > /dev/null; then # Install for debian-based linux distros
       sudo apt-get install -y whiptail
     elif command -v dnf > /dev/null; then # Install for newer RHEL-based linux distros
